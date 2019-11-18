692995eafb8876b14bb7781e98da0706284642a0
iidaissei <issei.288.higashi@icloud.com>
2019-11-18 12:35:39 +0900

    first_commit

---
+++ src/sample_navi_code.py
@@ -0,0 +1,81 @@
+######################################################################
+#アクションクライアントの例
+######################################################################
+
+#必要なインポート君たち
+import actionlib
+from  move_base_msgs.msg import MoveBaseAction, MoveBaseGoal
+from std_srvs.srv import Empty
+
+#コード例
+def navigationAC(coord_list):
+    try:
+        rospy.loginfo("Start Navigation")
+        ac = actionlib.SimpleActionClient('move_base', MoveBaseAction)
+        ac.wait_for_server()
+        #CostmapService
+        clear_costmaps = rospy.ServiceProxy('move_base/clear_costmaps', Empty)
+        #Set Goal
+        goal = MoveBaseGoal()
+        goal.target_pose.header.frame_id = 'map'
+        goal.target_pose.header.stamp = rospy.Time.now()
+        goal.target_pose.pose.position.x = coord_list[0]
+        goal.target_pose.pose.position.y = coord_list[1]
+        goal.target_pose.pose.orientation.z = coord_list[2]
+        goal.target_pose.pose.orientation.w = coord_list[3]
+        #Costmapを消去
+        rospy.wait_for_service('move_base/clear_costmaps')
+        clear_costmaps()
+        rospy.sleep(1.0)
+        #Goalを送信
+        ac.send_goal(goal)
+        state = ac.get_state()
+        count = 0#<---clear_costmapsの実行回数をカウントするための変数
+        while not rospy.is_shutdown():
+            state = ac.get_state()
+            if state == 1:
+                rospy.loginfo('Got out of the obstacle')
+                rospy.sleep(1.0)
+            elif state == 3:
+                rospy.loginfo('Navigation success!!')
+                return 'success'
+                state = 0
+            elif state == 4:
+                if count == 100:
+                    count = 0
+                    rospy.loginfo('Navigation Failed')
+                    return 'failed'
+                else:
+                    rospy.loginfo('Buried in obstacle')
+                    clear_costmaps()
+                    rospy.loginfo('Clear Costmaps')
+                    ac.send_goal(goal)
+                    rospy.loginfo('Send Goal')
+                    rospy.sleep(1.0)
+                    count += 1
+    except rospy.ROSInterruptException:
+        pass
+
+
+######################################################################
+#場所の座標をパラメータから読み込む部分
+######################################################################
+
+#必要なインポート君たち
+import sys
+from yaml import load
+
+#コード例
+def searchLocationName(target_name):
+    rospy.loginfo("Search LocationName")
+    #LocationListのyamlファイルを読み込む
+    f = open('/home/athome/catkin_ws/src/mimi_common_pkg/config/location_dict.yaml')
+    location_dict = load(f)
+    f.close()
+    if target_name in location_dict:
+        print location_dict[target_name]
+        rospy.loginfo("Retrun location_dict")
+        return location_dict[target_name]
+    else:
+        rospy.loginfo("Not found <" + target_name + "> in LocationDict")
+        return 'faild'
---
+++ src/subscriber.py
@@ -0,0 +1,49 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+#--------------------------------------------------------------------
+#Title: トピックを購読するためのサンプルROSノード
+#Author: Issei Iida
+#Date: 2019/09/29
+#Memo: トピック"/sample_topic"を購読して、数字が5の時ノード終了
+#--------------------------------------------------------------------
+
+#ROS関連ライブラリ
+import rospy
+from std_msgs.msg import String
+
+
+class SubscriberClass():
+    def __init__(self):
+        #Subscriber
+        #第一引数:トピック名 第二引数:メッセージ形名 第三引数:コールバック場所名（受け取る場所のこと）
+        self.sub_message = rospy.Subscriber('/sample_topic', String, self.messageCB)
+        self.data = 'none'
+
+    def messageCB(self, receive_msg):#<-------ここでトピックメッセージを受け取る
+        #.dataとはトピック'/sample_topic'のメッセージの階層のこと。
+        self.data = receive_msg.data
+
+    def printMessage(self):
+        while not rospy.is_shutdown():
+            if self.data == '5':
+                rospy.loginfo('Subscribe 5')
+                return 1 
+            else:
+                pass
+
+
+def main():
+    rospy.loginfo('Start "ex_subscriber"')
+    sc = SubscriberClass()
+    state = 0
+    while not rospy.is_shutdown():
+        state = sc.printMessage()
+        if state == 1:
+            rospy.loginfo('Finish "ex_subscriber"')
+            break
+        else:
+            pass
+
+if __name__ == '__main__':
+    rospy.init_node('ex_subscriber')
+    main()
